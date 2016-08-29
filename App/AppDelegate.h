fb3d9c40236b6787848759984f61644b80b8b314
main <jasper@mac-mini.fios-router.home>
2016-08-28 23:33:21 -0400

    Initial Commit

---
+++ App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  App
+//
+//  Created by Jasper on 8/28/16.
+//  Copyright © 2016 Jordan Jasper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ App/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  App
+//
+//  Created by Jasper on 8/28/16.
+//  Copyright © 2016 Jordan Jasper. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
